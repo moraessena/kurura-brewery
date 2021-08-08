5b72274a70338fb2dcfd2fa055c1ee794882eeac
daniel sena <danielsena04@gmail.com>
2021-08-08 20:55:25 -0300

    feat: add createBeer method on beer controller

--- src/main/java/br/com/kururabrewery/web/controller/BeerController.java
+++ src/main/java/br/com/kururabrewery/web/controller/BeerController.java
@@ -2,11 +2,10 @@ package br.com.kururabrewery.web.controller;
 
 import br.com.kururabrewery.service.BeerService;
 import br.com.kururabrewery.web.model.BeerDto;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,6 +22,12 @@ public class BeerController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("id") UUID id) {
         return ResponseEntity.ok(beerService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity createBeer(@RequestBody BeerDto beerDto) {
+        BeerDto savedDto = beerService.createBeer(beerDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedDto);
     }
 
 }
