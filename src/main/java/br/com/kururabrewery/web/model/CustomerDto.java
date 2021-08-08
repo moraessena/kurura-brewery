59e04be49b4d74a79393e4e1e374bf62a0887fbc
daniel sena <danielsena04@gmail.com>
2021-08-08 17:51:59 -0300

    feat: creating customer dto

---
+++ src/main/java/br/com/kururabrewery/web/model/CustomerDto.java
@@ -0,0 +1,17 @@
+package br.com.kururabrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDto {
+    private UUID id;
+    private String customerName;
+}
