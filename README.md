d31b35e2c838b83887c17d274343356b203d25e5
daniel sena <danielsena04@gmail.com>
2021-08-08 17:18:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kurura-brewery
---
+++ src/main/java/br/com/kururabrewery/KururaBreweryApplication.java
@@ -0,0 +1,13 @@
+package br.com.kururabrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KururaBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KururaBreweryApplication.class, args);
+	}
+
+}
