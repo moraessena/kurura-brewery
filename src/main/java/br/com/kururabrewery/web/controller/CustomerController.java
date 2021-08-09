665ec15a5b698259b23e376e1c306fe26934cc44
daniel sena <danielsena04@gmail.com>
2021-08-08 21:36:40 -0300

    feat: add create customer method on customer controller

--- src/main/java/br/com/kururabrewery/web/controller/CustomerController.java
+++ src/main/java/br/com/kururabrewery/web/controller/CustomerController.java
@@ -1,13 +1,14 @@
 package br.com.kururabrewery.web.controller;
 
 import br.com.kururabrewery.service.CustomerService;
+import br.com.kururabrewery.web.model.BeerDto;
 import br.com.kururabrewery.web.model.CustomerDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -23,6 +24,13 @@ public class CustomerController {
     @GetMapping("/{id}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("id") UUID id) {
         return ResponseEntity.ok(customerService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto beerDto) {
+        CustomerDto savedDto = customerService.createCustomer(beerDto);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedDto.getId()).toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).location(location).body(savedDto);
     }
 
 }
